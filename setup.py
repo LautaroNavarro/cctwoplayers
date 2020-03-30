08d3f1bd7a55a24d9c3d6c914b9dc94128e4dc7a
Lautaro <navarro_lautaro@hotmail.com>
2020-03-30 17:01:04 -0300

    Adding version info

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
     name='cctwoplayers',
     packages=['cctwoplayers'],
-    version='0.1.0',
+    version='0.2.0',
     license='gpl-3.0',
     description='Click counter two players game. Logic to handle the game, no UI is included. Easily to integrate with your prefered UI.',  # noqa
     author='Lautaro Navarro',
     author_email='navarro_lautaro@hotmail.com',
     url='https://github.com/LautaroNavarro/cctwoplayers',
-    download_url='https://github.com/LautaroNavarro/cctwoplayers/archive/0.1.0.tar.gz',  # noqa
+    download_url='https://github.com/LautaroNavarro/cctwoplayers/archive/0.2.0.tar.gz',  # noqa
     keywords=['GAME', 'CLICK', 'COUNTER'],
     install_requires=[],
     classifiers=[
